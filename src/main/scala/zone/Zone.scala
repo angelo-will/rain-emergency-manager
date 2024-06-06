d07b5406a7a6877315ff37db7f39a479ce3b5565
Angelo Tinti <angelo.tinti@studio.unibo.it>
2024-06-06 13:50:54 +0200

    Add to zone partial function for new pluviometer registered

--- src/main/scala/zone/Zone.scala
+++ src/main/scala/zone/Zone.scala
@@ -12,11 +12,15 @@ import scala.collection.mutable.Map
 object Zone:
   sealed trait Command extends Message
 
-  case class Alarm(pluvRef: ActorRef[Message]) extends Command
-
   case class PluviometerRegistered(name: String, replyTo: ActorRef[Message]) extends Command
 
+  case class Alarm(pluvRef: ActorRef[Message]) extends Command
+
   case class NoAlarm() extends Command
+
+  case class UnderManagement() extends Command
+
+  case class Solved() extends Command
 
   def apply(name: String): Behavior[Message] = Behaviors.setup { ctx =>
     ctx.log.info(s"Zone $name created")
@@ -27,7 +31,6 @@ private case class Zone(ctx: ActorContext[Message], name: String):
 
   import Zone.*
   import pluviometer.Pluviometer.UnsetAlarm
-  import firestastion.FireStation.DeactivateAlarm
   import scala.collection.mutable.Set
 
   private val pluviometers: Map[ActorRef[Message], Boolean] = Map()
@@ -37,27 +40,56 @@ private case class Zone(ctx: ActorContext[Message], name: String):
     this.working
 
   private def working: Behavior[Message] =
-    this.ctx.log.info("Entering working")
-    var alarmCount = 0
+    //    this.ctx.log.info("Entering working")
+    //    var alarmCount = 0
+    //
+    //    // To remove in the end
+    //    def debugAlarmCounter(): Unit = {
+    //      alarmCount = alarmCount + 1
+    //      if alarmCount >= 3 then
+    //        resetAlarm
+    //        alarmCount = 0
+    //    }
+
     Behaviors.receivePartial {
-      case (ctx, Alarm(pluvRef)) =>
-        ctx.log.info("RECEIVED ALARM")
-        pluviometers(pluvRef) = true
-        alarmCount = alarmCount + 1
-        if alarmCount >= 3 then
-          resetAlarm
-          alarmCount = 0
-        printPluvState
-        Behaviors.same
-      case (ctx, NoAlarm()) =>
-        ctx.log.info("RECEIVED NO ALARM")
-        printPluvState
-        Behaviors.same
-      case (ctx, PluviometerRegistered(name, replyTo)) =>
-        ctx.log.info(s"New pluv connected with name $name")
-        pluviometers.put(replyTo, false)
-        Behaviors.same
+      pluvRegistered(Behaviors.same).orElse {
+        case (ctx, Alarm(pluvRef)) =>
+          ctx.log.info("RECEIVED ALARM")
+          pluviometers(pluvRef) = true
+          //          debugAlarmCounter()
+          printPluvState
+          if this.isZoneInAlarm then inAlarm else Behaviors.same
+        case (ctx, NoAlarm()) =>
+          ctx.log.info("RECEIVED NO ALARM")
+          printPluvState
+          Behaviors.same
+      }
     }
+
+
+  private def inAlarm: Behavior[Message] =
+    Behaviors.receivePartial {
+      pluvRegistered(Behaviors.same).orElse {
+        case (ctx, UnderManagement()) =>
+          ctx.log.info("Received under management")
+          this.underManagement
+      }
+    }
+
+  private def underManagement: Behavior[Message] =
+    Behaviors.receivePartial {
+      case (ctx, Solved()) =>
+        ctx.log.info("Received solved")
+        this.resetAlarm
+        this.working
+    }
+
+  private def pluvRegistered(behavior: Behavior[Message]): PartialFunction[(ActorContext[Message], Message), Behavior[Message]] = {
+    case (ctx, PluviometerRegistered(name, replyTo)) =>
+      ctx.log.info(s"New pluv connected with name $name")
+      pluviometers.put(replyTo, false)
+      behavior
+  }
 
   private def resetAlarm =
     this.ctx.log.info(s"Sending unsetting alarm")
@@ -65,6 +97,9 @@ private case class Zone(ctx: ActorContext[Message], name: String):
       pluvRef ! UnsetAlarm(ctx.self)
       pluviometers(pluvRef) = false
 
-
+  private def isZoneInAlarm =
+    pluviometers.foldLeft(0) {
+      case (count, (_, value)) => if (value) count + 1 else count
+    } >= (pluviometers.size / 2)
 
   private def printPluvState = this.ctx.log.info(s"pluviometers state: $pluviometers")
