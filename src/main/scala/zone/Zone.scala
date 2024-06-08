22dbd2fdc0adccbaeed4c45687117ddbde9bf53a
Angelo Tinti <angelo.tinti@studio.unibo.it>
2024-06-08 14:21:38 +0200

    Add to zone handlers partial function and cluster member event (view full commit)
    Use partial functions to handle differents messages for all state, also introduce new handlers to manage members exiting from cluster.

--- src/main/scala/zone/Zone.scala
+++ src/main/scala/zone/Zone.scala
@@ -2,9 +2,10 @@ package zone
 
 import akka.actor.typed.scaladsl.{ActorContext, Behaviors}
 import akka.actor.typed.{ActorRef, Behavior}
-
+import akka.cluster.ClusterEvent.MemberExited
+import akka.cluster.typed.{Cluster, Subscribe}
 import message.Message
-import zone.Zone.Command
+import zone.Zone.{Command, MemberExitedAdapter}
 
 import scala.collection.mutable.Map
 
@@ -22,12 +23,30 @@ object Zone:
 
   case class Solved() extends Command
 
-  def apply(name: String): Behavior[Message] = Behaviors.setup { ctx =>
-    ctx.log.info(s"Zone $name created")
-    new Zone(ctx, name).creating
+  case class MemberExitedAdapter(event: MemberExited) extends Command
+
+  private def memberEventBehavior(zoneCtx: ActorContext[Message]) =
+    Behaviors.setup { ctx2 =>
+      Cluster(zoneCtx.system).subscriptions ! Subscribe(
+        ctx2.messageAdapter[MemberExited](MemberExitedAdapter.apply),
+        classOf[MemberExited]
+      )
+      Behaviors.receivePartial {
+        case (memberEventCtx, MemberExitedAdapter(event)) =>
+          memberEventCtx.log.info(s"MemberEventWrapper received with event ${event.member.address}")
+          zoneCtx.self ! MemberExitedAdapter(event)
+          Behaviors.same
+      }
+    }
+
+  def apply(name: String): Behavior[Message] = Behaviors.setup { zoneCtx =>
+    zoneCtx.spawn(memberEventBehavior(zoneCtx), "Member-event-actor")
+
+    zoneCtx.log.info(s"Zone $name created")
+    new Zone(name).creating
   }
 
-private case class Zone(ctx: ActorContext[Message], name: String):
+private case class Zone(name: String):
 
   import Zone.*
   import pluviometer.Pluviometer.UnsetAlarm
@@ -36,63 +55,71 @@ private case class Zone(ctx: ActorContext[Message], name: String):
   private val pluviometers: Map[ActorRef[Message], Boolean] = Map()
 
   private def creating: Behavior[Message] =
-    this.ctx.log.info("Entering creating")
+    //    this.ctx.log.info("Entering creating")
     this.working
 
   private def working: Behavior[Message] =
-    //    this.ctx.log.info("Entering working")
-    //    var alarmCount = 0
-    //
-    //    // To remove in the end
-    //    def debugAlarmCounter(): Unit = {
-    //      alarmCount = alarmCount + 1
-    //      if alarmCount >= 3 then
-    //        resetAlarm
-    //        alarmCount = 0
-    //    }
-
     Behaviors.receivePartial {
-      pluvRegistered(Behaviors.same).orElse {
-        case (ctx, Alarm(pluvRef)) =>
-          ctx.log.info("RECEIVED ALARM")
-          pluviometers(pluvRef) = true
-          //          debugAlarmCounter()
-          printPluvState
-          if this.isZoneInAlarm then inAlarm else Behaviors.same
-        case (ctx, NoAlarm()) =>
-          ctx.log.info("RECEIVED NO ALARM")
-          printPluvState
-          Behaviors.same
-      }
+      handlers(Behaviors.same)
+        .orElse {
+          case (ctx, Alarm(pluvRef)) =>
+            ctx.log.info(s"RECEIVED ALARM from $pluvRef")
+            ctx.log.info(s"RECEIVED ALARM from ${pluvRef.path.address}")
+            pluviometers(pluvRef) = true
+            printPluvState(ctx)
+            if this.isZoneInAlarm then inAlarm else Behaviors.same
+          case (ctx, NoAlarm()) =>
+            ctx.log.info("RECEIVED NO ALARM")
+            printPluvState(ctx)
+            Behaviors.same
+        }
     }
 
 
   private def inAlarm: Behavior[Message] =
     Behaviors.receivePartial {
-      pluvRegistered(Behaviors.same).orElse {
-        case (ctx, UnderManagement()) =>
-          ctx.log.info("Received under management")
-          this.underManagement
-      }
+      handlers(Behaviors.same)
+        .orElse {
+          case (ctx, UnderManagement()) =>
+            ctx.log.info("Received under management")
+            this.underManagement
+        }
     }
 
   private def underManagement: Behavior[Message] =
     Behaviors.receivePartial {
-      case (ctx, Solved()) =>
-        ctx.log.info("Received solved")
-        this.resetAlarm
-        this.working
+      handlers(Behaviors.same)
+        .orElse {
+          case (ctx, Solved()) =>
+            ctx.log.info("Received solved")
+            this.resetAlarm(ctx)
+            this.working
+        }
     }
 
   private def pluvRegistered(behavior: Behavior[Message]): PartialFunction[(ActorContext[Message], Message), Behavior[Message]] = {
     case (ctx, PluviometerRegistered(name, replyTo)) =>
       ctx.log.info(s"New pluv connected with name $name")
+      ctx.log.info(s"New pluv connected with ${replyTo.path.address}")
       pluviometers.put(replyTo, false)
       behavior
   }
 
-  private def resetAlarm =
-    this.ctx.log.info(s"Sending unsetting alarm")
+  private def memberExited(behavior: Behavior[Message]): PartialFunction[(ActorContext[Message], Message), Behavior[Message]] = {
+    case (ctx, MemberExitedAdapter(event)) =>
+      val actorRefToRemove = pluviometers.keys.find(_.path.address == event.member.address)
+      if actorRefToRemove.isDefined then pluviometers.remove(actorRefToRemove.get)
+      printPluvState(ctx)
+      behavior
+  }
+
+  private def handlers(behavior: Behavior[Message]): PartialFunction[(ActorContext[Message], Message), Behavior[Message]] = {
+    pluvRegistered(behavior)
+      .orElse(memberExited(behavior))
+  }
+
+  private def resetAlarm(ctx: ActorContext[Message]) =
+    ctx.log.info(s"Sending unsetting alarm")
     for ((pluvRef, _) <- pluviometers)
       pluvRef ! UnsetAlarm(ctx.self)
       pluviometers(pluvRef) = false
@@ -102,4 +129,4 @@ private case class Zone(ctx: ActorContext[Message], name: String):
       case (count, (_, value)) => if (value) count + 1 else count
     } >= (pluviometers.size / 2)
 
-  private def printPluvState = this.ctx.log.info(s"pluviometers state: $pluviometers")
+  private def printPluvState(ctx: ActorContext[Message]) = ctx.log.info(s"pluviometers state: $pluviometers")
