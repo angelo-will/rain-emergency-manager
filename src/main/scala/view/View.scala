07a173dddd1d409b5ab9dd2fc9fcad52cf8e6b8b
Angelo Tinti <angelo.tinti@studio.unibo.it>
2024-06-17 19:29:20 +0200

    Refactor view and view actor

--- src/main/scala/view/View.scala
+++ src/main/scala/view/View.scala
@@ -14,7 +14,7 @@ object FireStationGUI:
     case Busy
     case Free
 
-  def apply() = new FireStationGUI()
+  def apply(fsList: Seq[String]) = new FireStationGUI(fsList)
 
 case class FireStationStateComponent() extends BoxPanel(Orientation.Vertical):
 
@@ -39,17 +39,18 @@ case class FireStationStateComponent() extends BoxPanel(Orientation.Vertical):
     fireStationState.text = "Occupata"
   }
 
-
   def setZoneState(zoneState: ZoneStateGUI): Unit =
     zoneState match
       case ZoneStateGUI.Alarm => setZoneState("In allarme")
       case ZoneStateGUI.Managing => setZoneState("In gestione")
       case ZoneStateGUI.Ok => setZoneState("Sicura")
 
-  def setFireStationState(fireStationState: FireStationStateGUI): Unit =
+  def setFState(fireStationState: FireStationStateGUI): Unit =
     fireStationState match
       case FireStationStateGUI.Free => setFireStationState("Libera")
       case FireStationStateGUI.Busy => setFireStationState("Occupata")
+      
+  def setPluvQuantity(quantity: Int): Unit = sensors.text = ""+quantity
 
   private def setZoneState(state: String): Unit = zoneState.text = state
 
@@ -57,36 +58,46 @@ case class FireStationStateComponent() extends BoxPanel(Orientation.Vertical):
 
 end FireStationStateComponent
 
-case class FireStationGUI() extends SimpleSwingApplication {
+case class FireStationGUI(fireStationsCodes: Seq[String]) extends SimpleSwingApplication {
 
   import FireStationGUI.{ZoneStateGUI, FireStationStateGUI}
 
 
   // Create a container to hold multiple frames
   private val framesContainer = new BoxPanel(Orientation.Vertical)
-  private var panels: mutable.Seq[Component] = mutable.Seq()
-  private var fireStations: mutable.Map[String, FireStationStateComponent] = mutable.Map()
+  private val panels: mutable.Seq[Component] = mutable.Seq()
+  private val fireStations: mutable.Map[String, FireStationStateComponent] = mutable.Map()
+  private val mainPanel = new BoxPanel(Orientation.Vertical) {
+    contents += new Label("Stato delle caserma")
+    for fs <- fireStationsCodes do
+      println("Add new FS panel")
+      val fireS = FireStationStateComponent()
+      fireStations(s"firestation-$index") = fireS
+      contents += fireS
+    this.revalidate()
+    this.repaint()
+  }
+  
+  def setFSZState(fSCode: String, zoneStateGUI: ZoneStateGUI): Unit =
+    fireStations(fSCode).setZoneState(zoneStateGUI)
+  
+  def setFSState(fSCode: String, fireStationStateGUI: FireStationStateGUI): Unit =
+    fireStations(fSCode).setFState(fireStationStateGUI)
+    
+  def setPluvZoneQuantity(fSCode: String, quantity: Int): Unit =
+    fireStations(fSCode).setPluvQuantity(quantity)  
+  
   var index = 0
 
   def top: Frame = new MainFrame {
     title = "Visualizzazione zona e caserma"
-    contents = new BoxPanel(Orientation.Vertical) {
-      contents += new Label("Stato delle caserma")
-      contents += Button("Debug aggiungi caserma") {
-        println("Hai premuto il bottone!")
-        val fireS = FireStationStateComponent()
-        contents += fireS
-        fireStations(s"firestation-$index") = fireS
-        this.revalidate()
-        this.repaint()
-      }
-    }
-    size = new Dimension(300, 200)
+    contents = mainPanel
+//    size = new Dimension(300, 200)
   }
 
 }
 
 @main def aaa() =
-  FireStationGUI().main(Array())
+  FireStationGUI(Seq("fs-01","fs-02")).main(Array())
 
 
--- src/main/scala/view/ViewActor.scala
+++ src/main/scala/view/ViewActor.scala
@@ -1,30 +1,62 @@
 package view
 
-import akka.actor.typed.scaladsl.{Behaviors}
-import akka.actor.typed.{ActorRef, Behavior}
+
 import message.Message
 
 object ViewActor:
   sealed trait Command extends Message
 
-  def apply() = new ViewActor().running()
+  def apply(fsCodes: Seq[String]) =
+    val gui = FireStationGUI(fsCodes)
+    gui.main(Array())
+    new ViewActor(fsCodes, gui).start()
 
-case class ViewActor():
+case class ViewActor(fsCodes: Seq[String],gui: FireStationGUI):
 
-//  import firestastion.FireStation.FireStationState
-//  import view.FireStationGUI
-//  import view.FireStationGUI.FireStationStateGUI
+  import akka.actor.typed.pubsub.{Topic, PubSub}
+  import akka.actor.typed.scaladsl.{ActorContext, Behaviors}
+  import akka.actor.typed.{ActorRef, Behavior}
+
+  import scala.collection.mutable
+
+  import firestastion.FireStation.{FireStationState, MessageToActorView}
+  import zone.Zone.ZoneState
+
+  import view.FireStationGUI
+  import view.FireStationGUI.{FireStationStateGUI, ZoneStateGUI}
 
   val AAA = 0
+  var fireStations = mutable.Map[String, ActorRef[Message]]()
 
-  def running(): Behavior[Message] = Behaviors.setup { ctx =>
-    val gui = FireStationGUI()
-    gui.main(Array())
-//    Behaviors.receiveMessagePartial {
-//      case FireStationState.Free => gui.setFireStationState(FireStationStateGUI.Free); Behaviors.same
-//      case FireStationState.Busy => gui.setFireStationState(FireStationStateGUI.Busy); Behaviors.same
-//    }
-    Behaviors.empty
+  def start(): Behavior[Message] = Behaviors.setup { ctx =>
+
+//    for fs <- fsCodes do
+    ctx.spawn(listenerFireStation(ctx.self), s"listener-fs" )
+    debugBehav()
   }
-//      case FireStation. => gui.set(FireStation.State(z, f)); Behaviors.same
-//      case FireStation. => gui.set(FireStation.State(z, f)); Behaviors.same
+
+  def listenerFireStation(forwardActor: ActorRef[Message]) = Behaviors.setup { ctx =>
+    val pubSub = PubSub(ctx.system)
+    val topic: ActorRef[Topic.Command[Message]] = pubSub.topic[Message]("firestations-topic")
+    topic ! Topic.subscribe(ctx.self)
+    Behaviors.receiveMessagePartial {
+      case MessageToActorView(idFS,zoneState,fireStationState,refForReply) =>
+        updateFSState(idFS, fireStationState)
+        updateFSZState(idFS, zoneState)
+        Behaviors.same
+    }
+  }
+
+  def debugBehav(): Behavior[Message] =
+    Behaviors.receivePartial {
+      case (ctx, _) => Behaviors.same
+    }
+  def updateFSState(fsCode:String, fireStationState: FireStationState): Unit = fireStationState match
+    case FireStationState.Free => gui.setFSState(fsCode, FireStationStateGUI.Free)
+    case FireStationState.Busy => gui.setFSState(fsCode, FireStationStateGUI.Busy)
+
+  def updateFSZState(str: String, state: ZoneState): Unit = state match
+    case ZoneState.Alarm => gui.setFSZState(str, ZoneStateGUI.Alarm)
+    case ZoneState.Managing => gui.setFSZState(str, ZoneStateGUI.Managing)
+    case ZoneState.Ok => gui.setFSZState(str, ZoneStateGUI.Ok)
+
