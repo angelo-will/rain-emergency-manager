1e84c73fc4c42163424a65fe27573a7db2c3aa14
michele.bachetti <michele.bachetti@studio.unibo.it>
2024-06-23 18:41:03 +0200

    Feat: add inAlarm behaviour to fire station

--- src/main/scala/firestastion/FireStationActor.scala
+++ src/main/scala/firestastion/FireStationActor.scala
@@ -1,12 +1,12 @@
 package firestastion
 
 import akka.actor.typed.receptionist.{Receptionist, ServiceKey}
-import akka.actor.typed.scaladsl.{ActorContext, Behaviors}
+import akka.actor.typed.scaladsl.Behaviors
 import akka.actor.typed.{ActorRef, Behavior}
-import akka.cluster.pubsub.DistributedPubSubMediator.SubscribeAck
 import firestastion.FireStationActor.AdapterMessageForConnection
 import message.Message
-import systemelements.SystemElements.{FireStation, FireStationState, Zone, ZoneState}
+import systemelements.SystemElements.FireStationState.{Busy, Free}
+import systemelements.SystemElements.{FireStation, FireStationState, ZoneState}
 import zone.ZoneActor
 
 object FireStationActor:
@@ -26,15 +26,7 @@ object FireStationActor:
   case class SendGetStatus(zoneRef: ActorRef[Message]) extends Command
 
   //////
-  
   case class FireStationStatus(fireStationStatus: FireStation) extends Command
-
-//  case class MessageToActorView(
-//                                 fireStationId: String,
-//                                 zoneState: Option[ZoneState],
-//                                 fireStationState: FireStationState,
-//                                 refForReply: ActorRef[Message]
-//                               ) extends Command
 
   //adapter use for recover the zoneActor reference
   private case class AdapterMessageForConnection(listing: Receptionist.Listing) extends Command
@@ -45,9 +37,8 @@ object FireStationActor:
 
 private case class FireStationActor(name: String, fireStationCode: String, zoneCode: String):
   
-  import FireStationActor.{Managing, Solved, FindZone, ConnectedToZone, SendGetStatus, FireStationStatus}
+  import FireStationActor.{Managing, Solved, FindZone, SendGetStatus, FireStationStatus}
   import scala.concurrent.duration.FiniteDuration
-  import utilsduringdebug.PrintInfo
   import akka.actor.typed.pubsub.Topic
   import akka.actor.typed.pubsub.PubSub
 
@@ -55,7 +46,7 @@ private case class FireStationActor(name: String, fireStationCode: String, zoneC
   private val updateGUIConnectedFrequency = FiniteDuration(5, "second")
   private val askZoneStatus = FiniteDuration(5, "second")
   private var lastZoneState: Option[ZoneState] = None
-//  private var lastFireStationState: Option[FireStationState] = Some(FireStationState.Free)
+  private var fireState = Free
 
   private var zoneRef: Option[ActorRef[Message]] = None
 
@@ -77,8 +68,6 @@ private case class FireStationActor(name: String, fireStationCode: String, zoneC
           if (l.nonEmpty) {
             // found the zone, move to work
             operating(l.head)
-//            zoneRef = Some(l.head)
-//            Behaviors.same //CAMBIO CON NUOVO BEHAVIOUR DI QUANDO LAVORA
           } else Behaviors.same
       }
     }
@@ -98,7 +87,7 @@ private case class FireStationActor(name: String, fireStationCode: String, zoneC
         case ZoneActor.ZoneStatus(zoneClass, zoneRef) =>
           //send message to gui using pub/sub
           topic ! Topic.publish(
-            FireStationStatus(FireStation(fireStationCode, FireStationState.Free, zoneClass))
+            FireStationStatus(FireStation(fireStationCode, fireState, zoneClass))
           )
           zoneClass.zoneState match
             case ZoneState.Alarm => inAlarm(zoneRef)
@@ -107,115 +96,31 @@ private case class FireStationActor(name: String, fireStationCode: String, zoneC
     }
   }
 
-//  private def starting(zoneCode: String): Behavior[Message] = Behaviors.setup { ctx =>
-//    ctx.setLoggerName("" + ctx.self)
-//    PrintInfo.printActorInfo("From starting", ctx)
-//    val workerActor = ctx.spawn(waitConnectionToZone(), s"WORKER-FS-$fireStationCode")
-//    ctx.spawnAnonymous(connectToZone(zoneCode, ctx.self, workerActor))
-//    Behaviors.empty
-//    //    Behaviors.receiveMessagePartial {
-//    //      case ConnectedToZone(zoneRef) =>
-//    ////        val getStatusActor = ctx.spawn(getStatus(ctx.self, zoneRef), "actor-get-status")
-//    //        working(zoneRef)
-//    //    }
-//  }
+  private def inAlarm(zoneRef: ActorRef[Message]): Behavior[Message] = Behaviors.setup { ctx =>
 
-  private def debugEmptyBehavior() = Behaviors.receiveMessagePartial {
-    case _ => Behaviors.same
+    val pubSub = PubSub(ctx.system)
+    val topic: ActorRef[Topic.Command[Message]] = pubSub.topic[Message]("channel")
+
+    Behaviors.withTimers { timers =>
+      timers.startTimerAtFixedRate(SendGetStatus(zoneRef), askZoneStatus)
+      Behaviors.receiveMessagePartial {
+        case SendGetStatus(zoneRef) =>
+          zoneRef ! ZoneActor.GetZoneStatus(ctx.self)
+          Behaviors.same
+        case ZoneActor.ZoneStatus(zoneClass, zoneRef) =>
+          //send message to gui using pub/sub
+          topic ! Topic.publish(
+            FireStationStatus(FireStation(fireStationCode, fireState, zoneClass))
+          )
+          Behaviors.same
+        case Managing() =>
+          zoneRef ! ZoneActor.UnderManagement(ctx.self)
+          fireState = Busy
+          Behaviors.same
+        case Solved() =>
+          zoneRef ! ZoneActor.Solved(ctx.self)
+          fireState = Free
+          operating(zoneRef)
+      }
+    }
   }
-
-
-//  private def waitConnectionToZone(): Behavior[Message] = Behaviors.setup { ctx =>
-//    ctx.setLoggerName("" + ctx.self)
-//    ctx.log.info(s"Inside wait connection to zone")
-//    PrintInfo.printActorInfo("From waitConnectionToZone", ctx)
-//    Behaviors.receivePartial {
-//      case (ctx, ConnectedToZone(zoneRef)) =>
-//        PrintInfo.printActorInfo("From wait conn... ConnectedToZone received", ctx)
-//        //        ctx.log.info("From wait conn... ConnectedToZone received")
-//        working(zoneRef)
-//      case (ctx, message) =>
-//        ctx.log.info(s"received message $message")
-//        Behaviors.same
-//    }
-//  }
-
-//  private def working(zoneRef: ActorRef[Message]): Behavior[Message] =
-//    Behaviors.withTimers { timers =>
-//      timers.startTimerAtFixedRate(InformGui(), updateGUIConnectedFrequency)
-//      Behaviors.receivePartial {
-//        handlerInformGUIStatus(Behaviors.same)
-//          .orElse {
-//            case (ctx, _) =>
-//              ctx.log.info("Inside working")
-//              Behaviors.same
-//          }
-//      }
-//    }
-
-  private def inAlarm(zoneRef: ActorRef[Message]): Behavior[Message] =
-    Behaviors.receivePartial {
-      case (ctx, Managing()) =>
-        ctx.log.info("Received managing signal")
-        this.inManaging(zoneRef)
-    }
-
-  private def inManaging(zoneRef: ActorRef[Message]): Behavior[Message] =
-    Behaviors.receivePartial {
-      case (ctx, Solved()) =>
-        ctx.log.info("Situation solved")
-        this.operating(zoneRef)
-    }
-
-//  private def getStatus(parentActor: ActorRef[Message], zoneRef: ActorRef[Message]): Behavior[Message] =
-//    Behaviors.setup { ctx =>
-//      Behaviors.withTimers { timers =>
-//        timers.startTimerAtFixedRate(SendGetStatus(zoneRef), FiniteDuration(5, "s"))
-//        Behaviors.receiveMessagePartial {
-//          case SendGetStatus(zoneRef) =>
-//            zoneRef ! Zone.GetStatus(parentActor)
-//            Behaviors.same
-//          //          case ZoneInfo(zoneState, pluvQuantity) =>
-//          //            parentActor ! ZoneInfo(zoneState, pluvQuantity)
-//          //            Behaviors.same
-//        }
-//      }
-//    }
-//
-//  private def handlerZoneStatus(behavior: Behavior[Message]): PartialFunction[(ActorContext[Message], Message), Behavior[Message]] =
-//    case (ctx, ZoneInfo(zoneState, pluvQuantity)) => ???
-//
-//  private def handlerInformGUIStatus(behavior: Behavior[Message]): PartialFunction[(ActorContext[Message], Message), Behavior[Message]] =
-//    case (ctx, InformGui())
-//    =>
-//    //      PubSub(ctx.system).topic[Message](fireStationCode) !
-//    ???
-//
-//  private def connectToZone(zoneCode: String, parentRef: ActorRef[Message], actorToRegister: ActorRef[Message]) =
-//    Behaviors.setup { ctx =>
-//      ctx.setLoggerName(s"FireStation.connectToZone-${ctx.self}")
-//      PrintInfo.printActorInfoG("From connect to zone", ctx)
-//      Behaviors.withTimers { timers =>
-//        val zoneServiceKey = ServiceKey[Message](zoneCode)
-//        timers.startTimerAtFixedRate(FindZone(zoneCode), searchingZoneFrequency)
-//        Behaviors.receiveMessagePartial {
-//          case FindZone(zoneCode) =>
-//            PrintInfo.printActorInfoG("Timer end try to comunicate", ctx)
-//            // ctx.log.info("Timer end try to comunicate")
-//            ctx.system.receptionist ! Receptionist.Subscribe(zoneServiceKey, ctx.self)
-//            Behaviors.same
-//          case zoneServiceKey.Listing(l) =>
-//            PrintInfo.printActorInfoG("listing received, try connection", ctx)
-//            //            ctx.log.info("listing received, try connection")
-//            if l.nonEmpty then l.head ! Zone.RegisterFireStation(actorToRegister, ctx.self)
-//            Behaviors.same
-//          case Zone.ElementConnectedAck(zoneRef) =>
-//            PrintInfo.printActorInfoG("ElementConnectedAck Received", ctx)
-//            //            ctx.log.info("ElementConnectedAck Received")
-//            actorToRegister ! ConnectedToZone(zoneRef)
-//            Thread.sleep(1000)
-//            Behaviors.stopped
-//        }
-//      }
-//    }
-
