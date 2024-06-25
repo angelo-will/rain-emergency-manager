51c662daa6696c25e167d116f660f61d23e35f63
Elia Marcantognini <elia.marcantognini@gmail.com>
2024-06-25 18:32:10 +0200

    Implement actor system in GUI

--- src/main/scala/view/ViewActor.scala
+++ src/main/scala/view/ViewActor.scala
@@ -2,20 +2,16 @@ package view
 
 
 import akka.actor.typed.Behavior
-import akka.actor.typed.scaladsl.Behaviors
+import firestastion.FireStationActor.FireStationStatus
 import message.Message
-import systemelements.SystemElements.ZoneState
-
-import javax.swing.SwingUtilities
-import scala.concurrent.{ExecutionContext, Future}
-import scala.swing.Action
+import systemelements.SystemElements.{FireStation, ZoneState}
+import view.ViewListenerActor.ActionCommand.{END_INTERVENTION, INTERVENE, WAITING}
 
 object ViewActor:
   sealed trait Command extends Message
 
   def apply(fsCodes: Seq[String]): Behavior[Message] =
     new ViewActor(fsCodes).start()
-
 
 
 case class ViewActor(fsCodes: Seq[String]):
@@ -32,47 +28,62 @@ case class ViewActor(fsCodes: Seq[String]):
 
   import view.FireStationGUI.{FireStationStateGUI, ZoneStateGUI}
 
-  val AAA = 0
   private val fireStations = mutable.Map[String, ActorRef[Message]]()
   private val gui = FireStationGUI(fsCodes)
+  private var context: ActorContext[Message] = _
 
-  def start(): Behavior[Message] = Behaviors.setup { ctx =>
-//    ctx.spawn(listenerFireStation(ctx.self), s"listener-fs")
-    println("ViewActor started")
-      for fs <- fsCodes do
-        gui.addButtonListener(ViewListenerActor(fs, ctx.self), fs)
-      gui.main(Array.empty)
+  private def start(): Behavior[Message] = Behaviors.setup { ctx =>
+    ctx.spawn(listenerFireStation(ctx.self), s"listener-fs")
+    context = ctx
+    for fs <- fsCodes do
+      gui.setButtonAction(ViewListenerActor(fs, ctx, INTERVENE), fs)
+    gui.main(Array.empty)
     debugBehav()
   }
 
-  def listenerFireStation(forwardActor: ActorRef[Message]) = Behaviors.setup { ctx =>
-    val pubSub = PubSub(ctx.system)
-    val topic: ActorRef[Topic.Command[Message]] = pubSub.topic[Message]("firestations-topic")
+  private def listenerFireStation(forwardActor: ActorRef[Message]) = Behaviors.setup { ctx =>
+    val topic: ActorRef[Topic.Command[Message]] = PubSub(ctx.system).topic[Message]("firestations-topic")
     topic ! Topic.subscribe(ctx.self)
     Behaviors.receiveMessagePartial {
-      case FireStationActor.FireStationStatus(fireStation) =>
-        updateFSState(fireStation.fireStationCode, fireStation.fireStationState)
-        updateFSZState(fireStation.fireStationCode, fireStation.zone.zoneState)
+      case FireStationActor.Managing(fsCode) =>
+        println("Received Managing")
+        updateFSState(fsCode, FireStationState.Busy)
+        updateFSZState(fsCode, ZoneState.InManaging)
+        Behaviors.same
+      case FireStationActor.Solved(fsCode) =>
+        println("Received Solved")
+        updateFSState(fsCode, FireStationState.Free)
+        updateFSZState(fsCode, ZoneState.Ok)
+        Behaviors.same
+      case FireStationStatus(FireStation(fsCode, fireState, zoneClass)) =>
+        updateFSState(fsCode, fireState)
+        zoneClass.zoneState match
+          case ZoneState.Alarm =>
+            println("Received Alarm")
+            updateFSZState(fsCode, zoneClass.zoneState)
+          case _ =>updateFSState(fsCode, fireState)
+            updateFSZState(fsCode, zoneClass.zoneState)
         Behaviors.same
     }
   }
 
-  def debugBehav(): Behavior[Message] =
+  private def debugBehav(): Behavior[Message] =
     Behaviors.receivePartial {
       case (ctx, _) => Behaviors.same
     }
 
-  def updateFSState(fsCode: String, fireStationState: FireStationState): Unit = fireStationState match
+  private def updateFSState(fsCode: String, fireStationState: FireStationState): Unit = fireStationState match
     case FireStationState.Free => gui.setFSState(fsCode, FireStationStateGUI.Free)
     case FireStationState.Busy => gui.setFSState(fsCode, FireStationStateGUI.Busy)
 
-  def updateFSZState(str: String, zoneState: ZoneState): Unit =
+  private def updateFSZState(fsCode: String, zoneState: ZoneState): Unit =
     zoneState match
-      case ZoneState.Alarm => gui.setFSZState(str, ZoneStateGUI.Alarm)
-      case ZoneState.InManaging => gui.setFSZState(str, ZoneStateGUI.Managing)
-      case ZoneState.Ok => gui.setFSZState(str, ZoneStateGUI.Ok)
-
-
-@main def testActorGUI(): Unit =
-  val codes = Seq("fs-01", "fs-02", "fs-03", "fs-04")
-  ViewActor(codes)
+      case ZoneState.Alarm =>
+        gui.setFSZState(fsCode, ZoneStateGUI.Alarm)
+        gui.setButtonAction(ViewListenerActor(fsCode, context, INTERVENE), fsCode)
+      case ZoneState.InManaging =>
+        gui.setFSZState(fsCode, ZoneStateGUI.Managing)
+        gui.setButtonAction(ViewListenerActor(fsCode, context, END_INTERVENTION), fsCode)
+      case ZoneState.Ok =>
+        gui.setFSZState(fsCode, ZoneStateGUI.Ok)
+        gui.setButtonAction(ViewListenerActor(fsCode, context, WAITING), fsCode)
